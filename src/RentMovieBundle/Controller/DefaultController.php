95438f147c001b037e78d076138d09004e741c26
khrystyna <vercmakha@rambler.ru>
2015-02-19 12:43:38 +0100

    form added

--- src/RentMovieBundle/Controller/DefaultController.php
+++ src/RentMovieBundle/Controller/DefaultController.php
@@ -138,4 +138,9 @@ class DefaultController extends Controller
     {
         return $this->render('RentMovieBundle:Default:cartoon.html.twig', array());
     }
+	
+	public function formAction()
+    {
+        return $this->render('RentMovieBundle:Default:form.html.twig', array());
+    }
 }
